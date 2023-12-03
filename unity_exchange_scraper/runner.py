c0bcde6823ff6bea42910c30bd8ed0f0f25dc360
Akshay Waghmare <akshay.d.waghmare@gmail.com>
2023-12-03 17:16:49 +0530

    Succesfully fetching the bearer after login

--- unity_exchange_scraper/runner.py
+++ unity_exchange_scraper/runner.py
@@ -1,8 +1,16 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from unity_exchange_scraper.spiders.ball_running_spider import BallRunningSpiderSpider
+from spiders.ball_running_spider import BallRunningSpiderSpider
 
-process = CrawlerProcess(settings=get_project_settings())
+# Set up Scrapy project settings
+settings = get_project_settings()
+
+# Additional settings for Scrapy-Splash
+settings.set('SPLASH_URL', 'http://localhost:8050')  # Replace with your Splash instance URL
+settings.set('DUPEFILTER_CLASS', 'scrapy_splash.SplashAwareDupeFilter')
+settings.set('HTTPCACHE_STORAGE', 'scrapy_splash.SplashAwareFSCacheStorage')
+
+process = CrawlerProcess(settings)
 process.crawl(BallRunningSpiderSpider)
 process.start()
--- unity_exchange_scraper/spiders/ball_running_spider.py
+++ unity_exchange_scraper/spiders/ball_running_spider.py
@@ -1,10 +1,36 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
+import json
 
 class BallRunningSpiderSpider(scrapy.Spider):
     name = "ball_running_spider"
-    allowed_domains = ["unity-exchange-website.com"]
-    start_urls = ["http://unity-exchange-website.com/"]
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        # Define API URL, payload, and headers
+        
+        api_url = 'https://www.unityexch.com/v1/users/userLogin'
+        payload = {"userName":"akiaksdon","password":"unityexch1618","ip_info":"{\"ip\":\"103.199.192.50\",\"hostname\":\"AS17665 ONEOTT INTERTAINMENT LIMITED\",\"city\":\"Pune\",\"region\":\"Maharashtra\",\"country\":\"India\",\"loc\":18.6161,\"postal\":\"411001\",\"org\":\"AS17665 ONEOTT INTERTAINMENT LIMITED\"}"}
+        payload_bytes = json.dumps(payload).encode('utf-8')
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'Cookie': '_ga=GA1.1.183286417.1695652252; _ga_DG0Y8CDR0L=GS1.1.1701507880.36.1.1701509435.0.0.0',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Origin': 'https://www.unityexch.com',
+            'Referer': 'https://www.unityexch.com/'
+        }
+
+        # Create the Request object with custom headers and payload
+        yield scrapy.Request(
+            url=api_url,
+            method='POST',
+            headers=headers,
+            body=payload_bytes,  # Convert payload to JSON string
+            callback=self.parse_login_response,
+        )
+
+    def parse_login_response(self, response):
+        # Handle the API response
+        login_response = json.loads(response.text)
+        print(login_response)
